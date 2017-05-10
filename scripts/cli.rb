cd51e65aff9e72b8cbaec00182014711f8dc27c1
Matt Holmes <pdxmholmes@users.noreply.github.com>
2017-05-09 19:34:34 -0700

    Updates to the stack configs

--- scripts/cli.rb
+++ scripts/cli.rb
@@ -2,7 +2,7 @@ require 'optparse'
 
 options = {}
 parser = OptionParser.new do |opts|
-    opts.banner = 'Usage: hc-server [options]'
+    opts.banner = 'Usage: hc-server [options] command'
 
     opts.on('-t', '--target', 'Target infrastructure path. Defaults to CWD.') { |v| options[:target_path] = v || Dir.pwd }
 end
@@ -10,6 +10,7 @@ parser.parse!
 
 command = ARGV.pop
 if command.nil? then
+    puts 'Command is a required parameter'
     puts parser.to_s
     exit
 end
