6b8e9855dec84c919c166cc9230549c765087e93
Matt Holmes <pdxmholmes@users.noreply.github.com>
2017-05-09 18:47:18 -0700

    Switched Traefik to swarm mode Minor script updates

--- scripts/cli.rb
+++ scripts/cli.rb
@@ -1,13 +1,18 @@
 require 'optparse'
 
 options = {}
-OptionParser.new do |opts|
+parser = OptionParser.new do |opts|
     opts.banner = 'Usage: hc-server [options]'
 
     opts.on('-t', '--target', 'Target infrastructure path. Defaults to CWD.') { |v| options[:target_path] = v || Dir.pwd }
-end.parse!
+end
+parser.parse!
 
 command = ARGV.pop
+if command.nil? then
+    puts parser.to_s
+    exit
+end
 
-puts "Target: #{options[:target]}"
+puts "Target: #{options[:target_path]}"
 puts "Command: #{command}"
