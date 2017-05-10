2bc92317d57afc6e7057ec4225948fc15aa74dbb
Matt Holmes <pdxmholmes@users.noreply.github.com>
2017-05-09 18:32:55 -0700

    Fixed incorrect OptionParser constant

--- scripts/cli.rb
+++ scripts/cli.rb
@@ -1,7 +1,7 @@
 require 'optparse'
 
 options = {}
-OptionsParser.new do |opts|
+OptionParser.new do |opts|
     opts.banner = 'Usage: hc-server [options]'
 
     opts.on('-t', '--target', 'Target infrastructure path. Defaults to CWD.') { |v| options[:target_path] = v || Dir.pwd }
